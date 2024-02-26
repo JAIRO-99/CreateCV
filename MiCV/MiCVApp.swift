51407e430112dcf530d927415646b9101262e933
JAIRO-99 <jairo.lcelis1999@gmail.com>
2024-02-26 16:16:13 -0500

    Initial Commit

---
+++ MiCV/MiCVApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiCVApp.swift
+//  MiCV
+//
+//  Created by New on 26/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MiCVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
